512e19a9e08be31278debd9fcf3c1730539df2a4
Abhiroop Basak <abhiroopbasak.tech@gmail.com>
2025-11-19 08:34:21 +0100

    My updates

--- server/index.ts
+++ server/index.ts
@@ -58,7 +58,6 @@ app.use((req, res, next) => {
     serveStatic(app);
   }
 
-  // ALWAYS serve the app on port 5000
   // this serves both the API and the client
   const port = process.env.PORT || 5000;
 server.listen(port, () => {
